cc279e5097c1b5189167613c1a29eaf9f1f35236
pittpir <pittpir@gmail.com>
2019-01-17 15:28:01 -0600

    Redeisgned the MVC

--- junk2.js
+++ junk2.js
@@ -1,3 +1,5 @@
+
+
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -22,18 +24,48 @@ module.exports = app;
 app.set('view engine', 'ejs');
 const path = require('path');
 app.use(express.static(path.join(__dirname,'public')));
+//app.use(express.static('./public'));  //this will work
 
 // PORT
 const port = 3000;
 let data = require ("./models/marsMissions.js");
 
-//middleware portion
+//middleware
+function logger() {
+  console.log("Logging session data");
+}
+
 app.use(function (req, res, next) {
   let newDate = new Date(Date.now());
   console.log(`${newDate.toDateString()} ${newDate.toTimeString()}`);
   console.log("Home page hit by: " + req.connection.remoteAddress);
   next()
 })
+//app.use(logger);
+
+/*
+app.get('/missions', (req,res) => {
+  console.log("Home page hit by: " + req.connection.remoteAddress);
+  //res.send('<h1>Hello</h1>');
+  //res.render('pages/index.ejs',{title : title, names : names})
+  res.render('pages/index.ejs', { data });
+})
+*/
+//let me = "/cow";
+
+/*
+data.map(function (data1,index) {
+  createGet("/missions/" + data1.name, index);
+})
+
+function createGet (me,index) {
+app.get(me, (req,res) => {
+  let data1 = data[index];
+  res.render('pages/show.ejs', { data1 });
+})
+}
+*/
+
 
 app.get('/', (req,res) => {
   res.render('missions/index', {
@@ -44,12 +76,21 @@ app.get('/', (req,res) => {
 });
 
 app.get('/missions/:id', (req,res) => {
+  //console.log("Home page hit by: " + req.connection.remoteAddress);
+  //console.log(data[req.params.id]);
   res.render('missions/index', {
     data: data[req.params.id],
     page: 'show',   //file called list.ejs
     cssPath: '/css/style.css'  //point to the style sheet
   })
 });
+
+/*
+app.get('/missions/:index', (req,res) => {
+  let data1 = data[req.params.index];
+  res.render('pages/show.ejs', { data1 });
+})
+*/
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
@@ -65,3 +106,6 @@ app.get('/missions/:id', (req,res) => {
 app.listen(port, function() {
   console.log('Missions to Mars running on port: ', port);
 })
+
+
+
